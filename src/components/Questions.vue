af646ef1aed794e33a9453a4dcd7a989814f6b85
danielDumville <daniel.dumville@se.experis.com>
2021-08-10 15:13:58 +0200

    Added answer display and randomizing

--- src/components/Questions.vue
+++ src/components/Questions.vue
@@ -1,6 +1,9 @@
 <template>
     <div >
-        <p >{{nextQuestion.question}}  <button @click="showNextQuestion()">Next Question</button></p>
+        <p >{{nextQuestion.question}}  <button @click="showNextQuestion()" >Next Question</button>
+        <button @click="getDisplayAnswers()">Answers</button></p>
+        <p>{{nextQuestion.correct_answer}}</p>
+        <p>{{displayAnswers}}</p>
 
         
     </div>
@@ -18,7 +21,9 @@
                 question:'',
                 type:{},
                 correctAnswer:{},
-                incorrectAnswers:[]
+                incorrectAnswers:[],
+                displayAnswers:[]
+                
             }
         },
         methods: {
@@ -27,18 +32,34 @@
                // let response = await fetch("https://opentdb.com/api.php?amount=" + this.amountChoice + "&category=" + this.categoriesChoice+ "&difficulty="+this.difficultyChoice)
                 .then(response => response.json())
                 this.questions = response.results
+                
+            },
+            getDisplayAnswers(){
+                for (let index = 0; index < this.incorrectAnswers.length; index++) {
+                    this.displayAnswers.push(this.incorrectAnswers[index])
+                }
+                this.displayAnswers.push(this.correctAnswer)
+                this.displayAnswers.sort(()=>Math.random()-0.5)
+
             },
             showNextQuestion(){
+                 this.displayAnswers = []
                 if(this.questions.length < 1) {
-                    //No questions remain
+                    //No questions remain, progress to next screen
                     console.log("No questions remaining")
                 }else{
                     this.nextQuestion = this.questions[Math.floor(Math.random()*this.questions.length)]
+                    this.incorrectAnswers = this.nextQuestion.incorrect_answers
+                    this.correctAnswer = this.nextQuestion.correct_answer
                     this.questions.pop(this.nextQuestion)
                     console.log(this.nextQuestion) 
+                   
+                    this.getDisplayAnswers()
+
                 }
                 
             }
+            
 
         },
         created(){
